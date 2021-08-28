449be89b1bcfc477df1a2f5fae66823bea0a2d8c
wthoutjc <juancamilorr2012@hotmail.com>
2021-08-28 16:09:18 -0500

    inicio v1

--- app.py
+++ app.py
@@ -1,49 +1,44 @@
 from flask import Flask, request, render_template, jsonify, make_response, after_this_request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField, FileAllowed
-from wtforms.validators import ValidationError
+from wtforms import SubmitField
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 import json
 from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
+#Config del entorno
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 #Creacion de forms
 class LoadImageForm(FlaskForm):
-    image = FileField('Load image', validators=[FileAllowed(['tiff'])])
-    submit = SubmitField('Submit')
+    photo = FileField('Load Image', validators=[
+        FileRequired(),
+        FileAllowed(['png', 'pdf', 'jpg'], "wrong format!")
+    ])
 
 #Rutas visuales
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def load_main():
     '''
     Cargamos el index.html primera parte visual
     '''
     main_form = LoadImageForm()
+    if request.method == "POST":
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(main_form.upload.data.filename))
+        main_form.upload.data.save(file_path)
+        return redirect(url_for('dashboard'))
+    return render_template("inicio.html", form = main_form) #Inicio
 
-    if main_form.validate_on_submit():
-        filename = secure_filename(main_form.file.data.filename)
-        main_form.file.data.save('uploads/' + filename)
-        return redirect(url_for('load_app'))
-    return render_template("inicio.html", data = main_form) #Inicio
 
-
-@app.route("/app")
-def load_app():
+@app.route("/app", methods=["POST"])
+def dashboard():
     '''
     Cargamos el app.html segunda parte visual
     '''
     return render_template("app.html") #App
-
-@app.route("/verify-image", methods=["GET", "POST"]) #App
-def verify_image():
-    '''
-    Verificamos la imagen
-    '''
-    @after_this_request
-    def add_header(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return render_template("app.html")
-    return render_template("app.html")
 #Rutas verificar
 
 #@app.route("/") #Load image
